a44486e2b27506f4ca3471b59e76eda52dfae146
haroun39 <haroun200239@gmail.com>
2024-02-01 11:59:17 +0100

    change image data

--- src/data/explorer-data.tsx
+++ src/data/explorer-data.tsx
@@ -1,6 +1,6 @@
 export const data = [
   {
-    img: "https://s3-alpha-sig.figma.com/img/62e4/c825/76552de757b001449cfea38c31844ed6?Expires=1706486400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=fYOQOfREawZPr7xuV9RUB01XEbHxzo~KvKuaFNX94ReP~5mGf4K-I78HgmEpB4jxVa5X40MZUwOJ7Uba9CBj8J1xhS8ClYIwcN4N4MzEf1wZ47KNrC4BuE8rn4zHlhoPE89uALnop0mJHUpXOgd5Gaoo6aWOfLppkH1VDvFu498qeOBr7vC4VJnW52OSPVnb7YvwOanNd733bCj1eM8CG0d2x1BftF-yOsXg12BheGt3MKuNKK-UJTLFbJjElAu89xO8aqnLoom8ihJ8--k7dilWieK7v3ObcK9MlpE5RdZ~XQVakv4lt4ElVuMElGSp5ZEFWmKRhBcC6GAf1EBx5Q__",
+    img: "assets/image1.png",
     title: "WH Questions",
     teacher: "Dr.Nom",
     views: "4.9k vues",
@@ -13,7 +13,7 @@ export const data = [
     tags: ["Math", "SNC", "3eme"],
   },
   {
-    img: "https://s3-alpha-sig.figma.com/img/9e2a/26fb/a62583293d5afc88b10b7747c2b5b1ca?Expires=1706486400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=l5LCbhUVMwKq3cEWMe3q9ODj-2Od7Q-ljRLSjbGDQM5GqEGAgIa53yKApAj-2LXjMuZtn0K8C3y2R9m3OPeMHsLAPac4e3c5zYrvNP73If1cWyI2nuYA9CsX9jSV~N6H2B1lcd2gGFAxicOWiLiz8u~v-JScCwWDkfdQeC30K~IZK7uPnGuzFqyQTbG1nr5xIZhXOnc6-wozpfwzbaHtnbFyPI0Hf-kNNnfJr1vjK9DisJ7YtGJ8sazl6kN8XW7qmKQon36WV-yXqBQbkgH~UNKRlCW6Sj676hgsRbm1Nc6k7www2940ucBceOWN~blH3P1bpMqjE7w8KjF89B8~lQ__",
+    img: "assets/image2.png",
     title: "Les fonctions f(x)",
     teacher: "Dr.Nom",
     views: "4.9k vues",
@@ -26,7 +26,7 @@ export const data = [
     tags: ["Math", "SNC", "3eme"],
   },
   {
-    img: "https://s3-alpha-sig.figma.com/img/bc26/8ec0/8ded2c1aa1075c793de37b917dbbdeea?Expires=1706486400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=KFPckNpWcCbLj2EqPqlbYILGg6Gw~lDDdUEyCNwHtZF50SziqTxZowGtqdj20zWczDxkc-FPrLKfX8MZATosQ-J0APJ6DrlsO~9OTNdnOmaMEol0weXyYa02b6SGGktSca4GvekzZmxbwJjyRuruy4t5SqKe3dgc0SCvnEq4~5GC5bOwqy4ehYyv6b3AIrluw4t9Jo~bWH-UPf-IBm2qRKnWTHUVvFWpaLO2kY~U4jEGATQ5Uq3ecErBhl6QT04zOown~HDwxHXZrkT7QcBBxTpPI2~3LRN~c83ETy~li5Ml-1QHeq~DOq4WyxR6dKP7pvqf0mp~FuQAKeejuhs4zg__",
+    img: "assets/image3.png",
     title: "Arab cours revisions des livres",
     teacher: "Dr.Nom",
     views: "4.9k vues",
@@ -39,7 +39,7 @@ export const data = [
     tags: ["Math", "SNC", "3eme"],
   },
   {
-    img: "https://s3-alpha-sig.figma.com/img/c2a0/6eeb/0d3825f931da6cbff86bb5a3fc08b4d8?Expires=1706486400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=SW2~~NtYvbuQJEBsqzI4REQ9PZrUC2XjEj3wUF0MHn6IiYwxQDSComnCWWsdnKrv9Zr6aDirSlwaa2ys5782Vu2CiTxexiep8J5He8enjo3BT2s3-BHMOsZhZe3ZTVdCVS-K5aZzRHFPtp43XSzAo4jjDv4WoV0mu4zK9cSzg96widhtwvvgB7EHZR3XxFsIh6l3jdFHICeKI75COEHutTEQ5Cq811avWm5JPsqaGAVv3wm~zHhPiJBQj3wAUmwrjK9LLvKGKKi1u4iPJOEkN59PBdEVVuKagPFXj~yplfFuEKjcrAoQcQvhmN9sw-evFV2sj4Y42E4VswSOKlkcgg__",
+    img: "assets/image4.png",
     title: "Cours de français",
     teacher: "Dr.Nom",
     views: "4.9k vues",
@@ -52,7 +52,7 @@ export const data = [
     tags: ["Math", "SNC", "3eme"],
   },
   {
-    img: "https://s3-alpha-sig.figma.com/img/a0ca/9979/79f64a5c0c0fde0b4b1eb67e2c21daf2?Expires=1706486400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=XoNV1CoA46M-QRrqbSGKOaXY-MSJ3Sqq7FdDvP~nicnTPlM-Imk6vebBp9zvaSyNIswgvnmN~oJzUZvhvI1AU19~-H3DzPeMd26iQthfumHPx81RrHJI1BvxUje3LlhEdDx30gCvk45FSo4meq~bR-iuD09ffxN6a3YLvMg9gf35RvmQQ2rGU3WrYUftFJd92gFK12Vtg-OrLXknQZsb7HW9kNKlO6l2FzpVi-2rRj7JvplNNzCjMWBufWqwYjNIGNbihnr381hYqSQlhrKuMHQiXw92fuAIWnRwGgfD0J9Fld6rszotkDZwGwGBZZVDJDl3sbCtuNSMCR4h4z6cJw__",
+    img: "assets/image5.png",
     title: "WH Questions",
     teacher: "Dr.Nom",
     views: "4.9k vues",
